46d96a6ae45d48fd3add4d8b106f0d157304c1ab
sspony <ceyda.ansin@yahoo.com>
2017-02-14 20:45:52 -0500

    Add files via upload

---
+++ milestones/milestone1/wpgeneral.h
@@ -0,0 +1,16 @@
+// OOP244 Milestone 1:	iostream
+// File:				wpgeneral.h
+// Version:				1
+// Date:				Nov 4, 2016
+/////////////////////////////////////////////
+
+#ifndef ICT_GENERAL_H__
+#define ICT_GENERAL_H__
+
+#define TAX 0.13
+#define MAX_UPC_LEN 7
+#define MIN_YEAR 2000
+#define MAX_YEAR 2030
+#define MAX_NO_RECS 2000
+
+#endif
